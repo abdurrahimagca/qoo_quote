a3ae4f147f7b6487dc7af0adf6d442b88c97511c
PolatAtak <yazicidevriim@gmail.com>
2025-04-11 20:39:19 +0300

    apo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qoo_quote/screens/login.dart';
 import 'package:qoo_quote/store/auth_store.dart';
 import 'app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -8,12 +9,15 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
-      child: const QooQuoteApp(),
-    ),
-  );
+  runApp(MaterialApp(
+    home: LoginPage(),
+  )
+
+      // MultiProvider(
+      //   providers: [
+      //     ChangeNotifierProvider(create: (_) => AuthProvider()),
+      //   ],
+      //   child: const QooQuoteApp(),
+      // ),
+      );
 }
